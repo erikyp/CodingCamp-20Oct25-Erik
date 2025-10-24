c319cf2ec3e801dccb19f38ed800b0f9439a82fd
Erik <erikypratama@gmail.com>
2025-10-24 17:12:21 +0700

    nav bar seems to work accpeptably now

--- js/script.js
+++ js/script.js
@@ -1,12 +1,65 @@
 class Me {
-  constructor() { }
-  
-  initilize() {
-    console.log(`initilized...`);
+  constructtor() {
+    this.debug_mode = true;
   }
 
-  asking_name(){
-    console.log(`name?`);
+  begin() {
+    this.initialize();
+    this.start();
+  }
+
+  #log(msg) {
+    if (this.debug_mode) {
+      console.log(msg);
+    }
+  }
+
+  initialize() {
+    this.#log(`initilized...`);
+
+    //#region set navbar behavior
+    document.addEventListener('DOMContentLoaded', () => {
+      const burger = document.querySelector('.bulma-navbar-burger');
+      const menu = document.getElementById(burger.dataset.target);
+
+      burger.addEventListener('click', () => {
+        burger.classList.toggle('bulma-is-active');
+        menu.classList.toggle('bulma-is-active');
+      });
+    });
+    //#endregion
+
+  }
+
+  start() {
+    this.#log(`start...`);
+
+    this.asking_name();
+  }
+
+  asking_name() {
+    this.#log(`asking name...`);
+  }
+
+
+  home() {
+    this.#log(`home clicked...?`);
+
+  }
+
+  our_profile() {
+    this.#log(`our profile clicked...?`);
+
+  }
+
+  portfolio() {
+    this.#log
+
+  }
+
+  message_us() {
+    this.#log(`message us clicked...?`);
+
   }
 }
 
